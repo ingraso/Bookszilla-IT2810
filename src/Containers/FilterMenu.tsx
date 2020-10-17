c6756207270c59b3abfcc1c1c25a3a3fe0f785af
Karolill <56150049+Karolill@users.noreply.github.com>
2020-10-17 11:50:48 +0200

    #14 - implemented layout for main page on computers

---
+++ src/Containers/FilterMenu.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Checkbox from "../Atoms/Checkbox";
+import "../Styles/FilterMenu.css";
+
+/**
+ * FilterMenu is is a component that shows the different Checkboxes you can
+ * choose between.
+ */
+
+const FilterMenu = () => {
+  return (
+    <div id="filter-menu">
+      <label>LANGUAGE</label>
+      <Checkbox text="English" />
+      <Checkbox text="Spanish" />
+
+      <label>GENRE</label>
+      <Checkbox text="Romace" />
+      <Checkbox text="Horror" />
+
+      <label>PUBLISHER</label>
+      <Checkbox text="Wiley" />
+      <Checkbox text="Aschehoug" />
+    </div>
+  );
+};
+
+export default FilterMenu;
