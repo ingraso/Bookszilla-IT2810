c7ecf42885efb943267e8a92fff2fe22bae00bc8
Ingrid Amalie Solbjørg <ingraso@stud.ntnu.no>
2020-10-20 13:25:49 +0200

    #12 Display detailed book on click

--- src/containers/BookDetails.tsx
+++ src/containers/BookDetails.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { MdClose } from "react-icons/md";
+import store from "../redux/store";
 import "../styles/BookDetails.css";
 
 /**
@@ -9,8 +10,25 @@ import "../styles/BookDetails.css";
 
 const BookDetails = () => {
   const [bookDetailsClassName, setBookDetailsClassName] = useState<string>(
-    "opened-book"
+    "closed-book"
   );
+  // remember to remove this after retrieving data from db
+  let title: string = "The Spice Shelf Girls";
+  let author: string = "Xu Wang";
+  let cover: string = "../../sunset.jpg";
+  let genres: Array<string> = ["Drama", "Documentary"];
+
+  store.subscribe(() => displayDetailedView());
+
+  const displayDetailedView = () => {
+    setBookDetailsClassName("opened-book");
+    // if id !== 0 => get data for book with id 'props.id' and display this
+    console.log(store.getState()); // to show that another book cover is clicked
+  };
+
+  const notImplemented = () => {
+    alert("Not yet implemented :(");
+  };
 
   return (
     <div className={bookDetailsClassName} id="book-details">
@@ -20,8 +38,37 @@ const BookDetails = () => {
       >
         <MdClose size="20px" />
       </button>
-      [Sett inn detaljer om boken her. Book-details er en flexboks for å sette
-      inn ting i, med flex-direction column. Btw: boksen scroller ved overflow]
+      <img src={cover} alt="SOME book cover" />
+      <p className="title">Title: {title}</p>
+      <p className="author">Author: {author}</p>
+      <p className="genre">
+        Genres:
+        {genres.map((genre) => {
+          return <b key={genre}> {genre}</b>;
+        })}
+      </p>
+
+      <button
+        id="favorite-button"
+        className="red-button"
+        onClick={notImplemented}
+      >
+        Favorite
+      </button>
+      <button
+        id="wish-to-read-button"
+        className="red-button"
+        onClick={notImplemented}
+      >
+        Wish to read
+      </button>
+      <button
+        id="have-read-button"
+        className="red-button"
+        onClick={notImplemented}
+      >
+        Have read
+      </button>
     </div>
   );
 };
