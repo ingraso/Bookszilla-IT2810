e05336a2ac39f2a63dd5dce9936bb2f88978600f
Ingrid Amalie Solbjørg <ingraso@stud.ntnu.no>
2020-10-26 11:39:37 +0100

    #5 Re-commit created forms after gitignoring node_modules

--- frontend/src/atoms/Book.tsx
+++ frontend/src/atoms/Book.tsx
@@ -1,6 +1,6 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { changeDetailedBook } from "../redux/actions";
-import store from "../redux/store";
 import "../styles/Book.css";
 
 interface BookProps {
@@ -10,20 +10,23 @@ interface BookProps {
   cover: string;
 }
 
-/*const updateDetailedBook = (id: number) => {
-  store.dispatch(changeDetailedBook(id));
-};*/
-
 /**
  * Book is a representation of a book, with its title,
  * author and cover image.
  * @param props
  */
 export const Book = (props: BookProps) => {
+  const dispatch = useDispatch();
+
+  const updateDetailedBook = (id: number) => {
+    dispatch(changeDetailedBook(id));
+  };
+
   return (
     <div
       id={String(props.id)}
       className="single-book"
+      onClick={() => updateDetailedBook(props.id)}
     >
       <img src={props.cover} alt="`SOME book cover" />
       <p className="title">{props.title}</p>
