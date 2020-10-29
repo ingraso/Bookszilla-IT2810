961e51213a700a8ae1bba3f34e67880b3a8058ab
Karolill <56150049+Karolill@users.noreply.github.com>
2020-10-29 14:22:37 +0100

    #23 - changed how bookd-details appear

--- frontend/src/containers/BookDetails.tsx
+++ frontend/src/containers/BookDetails.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { MdClose } from "react-icons/md";
 import store from "../redux/store";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { changeDetailedBook } from "../redux/actions";
 import "../styles/BookDetails.css";
 import { useQuery } from "@apollo/client";
 import { GET_BOOK_BY_ID } from "../assets/bookHandling";
@@ -13,15 +14,15 @@ import { GET_BOOK_BY_ID } from "../assets/bookHandling";
  */
 
 const BookDetails = () => {
-  const [bookDetailsClassName, setBookDetailsClassName] = useState<string>(
-    "closed-book"
-  );
-
   const phonePage: any = useSelector((state: any) => state.phonePage.phonePage);
-
   const bookId: any = useSelector((state: any) => state.id.id);
+  const dispatch = useDispatch();
 
-  const { loading, error, data } = useQuery(GET_BOOK_BY_ID, {
+  const changeBookIdToEmptyString = () => {
+    dispatch(changeDetailedBook(""));
+  };
+
+  const { data } = useQuery(GET_BOOK_BY_ID, {
     variables: { id: bookId },
   });
 
@@ -37,67 +38,67 @@ const BookDetails = () => {
     genres = data?.bookById?.genres;
   }
 
-  // should only subscribe to changes in detailedBookId - because
-  // now I think it will display a book on other changes in the store as well
-  store.subscribe(() => displayDetailedView());
-
   const loginStatus: boolean = useSelector(
     (state: any) => state.loginStatus.loginStatus
   );
-
-  const displayDetailedView = () => {
-    setBookDetailsClassName("opened-book");
-  };
 
   const notImplemented = () => {
     alert("Not yet implemented :(");
   };
 
-  return (
-    <div className={bookDetailsClassName + " " + phonePage} id="book-details">
-      <button
-        className="close-button"
-        onClick={() => setBookDetailsClassName("closed-book")}
+  if (bookId !== "") {
+    return (
+      <div
+        id="book-details"
+        className={/*bookDetailsClassName + " " + */ phonePage}
       >
-        <MdClose size="20px" />
-      </button>
-      <img src={cover} alt="SOME book cover" />
-      <p className="title">Title: {title}</p>
-      <p className="author">Author: {author}</p>
-      <p className="genre">
-        Genres:
-        {genres.map((genre) => {
-          return <b key={genre}> {genre}</b>;
-        })}
-      </p>
+        <button
+          id="close-book-details"
+          className="close-button"
+          onClick={() => changeBookIdToEmptyString()}
+        >
+          <MdClose size="20px" />
+        </button>
+        <img src={cover} alt="SOME book cover" />
+        <p className="title">Title: {title}</p>
+        <p className="author">Author: {author}</p>
+        <p className="genre">
+          Genres:
+          {genres.map((genre) => {
+            return <b key={genre}> {genre}</b>;
+          })}
+        </p>
 
-      {loginStatus ? (
-        <>
-          <button
-            id="favorite-button"
-            className="red-button"
-            onClick={notImplemented}
-          >
-            Favorite
-          </button>
-          <button
-            id="wish-to-read-button"
-            className="red-button"
-            onClick={notImplemented}
-          >
-            Wish to read
-          </button>
-          <button
-            id="have-read-button"
-            className="red-button"
-            onClick={notImplemented}
-          >
-            Have read
-          </button>
-        </>
-      ) : null}
-    </div>
-  );
+        {loginStatus ? (
+          <>
+            <button
+              id="favorite-button"
+              className="red-button"
+              onClick={notImplemented}
+            >
+              Favorite
+            </button>
+            <button
+              id="wish-to-read-button"
+              className="red-button"
+              onClick={notImplemented}
+            >
+              Wish to read
+            </button>
+            <button
+              id="have-read-button"
+              className="red-button"
+              onClick={notImplemented}
+            >
+              Have read
+            </button>
+          </>
+        ) : null}
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
 };
 
 export default BookDetails;
